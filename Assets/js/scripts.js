9017d73de99371b0952cbad90c4e2542de806602
Devon <devonross50@yahoo.com>
2021-04-02 12:36:58 -0500

    added day.js and formatted for time based color coding

--- Assets/js/scripts.js
+++ Assets/js/scripts.js
@@ -1,3 +1,135 @@
+var time = dayjs().format('H');
+console.log(time);
+var time=8;
+
+
+//9 am pull value from localStorage
+var storage9AM = localStorage.getItem("nineAM");
+var p9AM = $('.nineAM').find('p')[0];
+$(p9AM).text(storage9AM);
+if(time<9){
+    $(".nineAM").addClass('future');
+}
+if(time===9){
+    $(".nineAM").addClass('present');
+}
+if(time>9){
+    $(".nineAM").addClass('past');
+}
+
+//10 am pull from localStorage
+var storage10AM = localStorage.getItem("tenAM");
+var p10AM = $('.tenAM').find('p')[0];
+$(p10AM).text(storage10AM);
+//10am change class for formatting
+if(time<10){
+    $(".tenAM").addClass('future');
+}
+if(time===10){
+    $(".tenAM").addClass('present');
+}
+if(time>10){
+    $(".tenAM").addClass('past');
+}
+
+//11 am pull from localStorage
+var storage11AM = localStorage.getItem("elevenAM");
+var p11AM = $('.elevenAM').find('p')[0];
+$(p11AM).text(storage11AM);
+//11am change class for formatting
+if(time<11){
+    $(".elevenAM").addClass('future');
+}
+if(time===11){
+    $(".elevenAM").addClass('present');
+}
+if(time>11){
+    $(".elevenAM").addClass('past');
+}
+
+//12pm pull from localStorage
+var storage12PM = localStorage.getItem("twelvePM");
+var p12PM = $('.twelvePM').find('p')[0];
+$(p12PM).text(storage12PM);
+if(time<12){
+    $(".twelvePM").addClass('future');
+}
+if(time===12){
+    $(".twelvePM").addClass('present');
+}
+if(time>12){
+    $(".twelvePM").addClass('past');
+}
+
+//1PM pull from localStorage
+var storage1PM = localStorage.getItem("onePM");
+var p1PM = $(".onePM").find("p")[0];
+$(p1PM).text(storage1PM);
+if(time<13){
+    $(".onePM").addClass('future');
+}
+if(time===13){
+    $(".onePM").addClass('present');
+}
+if(time>13){
+    $(".onePM").addClass('past');
+}
+
+//2PM pull from localStorage
+var storage2PM = localStorage.getItem("twoPM");
+var p2PM = $(".twoPM").find("p")[0];
+$(p2PM).text(storage2PM);
+if(time<14){
+    $(".twoPM").addClass('future');
+}
+if(time===14){
+    $(".twoPM").addClass('present');
+}
+if(time>14){
+    $(".twoPM").addClass('past');
+}
+
+//3PM pull from localStorage
+var storage3PM = localStorage.getItem("threePM");
+var p3PM = $(".threePM").find("p")[0];
+$(p3PM).text(storage3PM);
+if(time<15){
+    $(".threePM").addClass('future');
+}
+if(time===15){
+    $(".threePM").addClass('present');
+}
+if(time>15){
+    $(".threePM").addClass('past');
+}
+
+//4PM pull from localStorage
+var storage4PM = localStorage.getItem("fourPM");
+var p4PM = $(".fourPM").find("p")[0];
+$(p4PM).text(storage4PM);
+if(time<16){
+    $(".fourPM").addClass('future');
+}
+if(time===16){
+    $(".fourPM").addClass('present');
+}
+if(time>16){
+    $(".fourPM").addClass('past');
+}
+
+//5PM pull from localStorage
+var storage5PM = localStorage.getItem("fivePM");
+var p5PM = $(".fivePM").find("p")[0];
+$(p5PM).text(storage5PM);
+if(time<17){
+    $(".fivePM").addClass('future');
+}
+if(time===17){
+    $(".fivePM").addClass('present');
+}
+if(time>17){
+    $(".fivePM").addClass('past');
+}
 
 //9AM functions
 $(".nineAM").on("click", function(){
